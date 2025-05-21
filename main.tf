1fc5a133086960283d182915f31ff29eece6f9e7
Johannes Scheuerer <joscheuerer@gmail.com>
2025-05-21 11:38:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,4 +40,31 @@ resource "aws_instance" "example" {
    Name = "EC2-with-3-EBS"
    application_acronym = "TTO"
  }
- }
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+ 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+ 
+  tags = {
+    application_acronym = "TTO"
+  }
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
